943ca21902679d86c4ff7da23fece6a5bf7d67c6
Martin Lopez <martin.lopezx@neoris.com>
2020-11-25 13:13:06 -0300

    documentando codigo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,78 +10,73 @@ function Login() {
     const [response, setResponse] = useState({});
 
 
-//esta función completa el objeto del request para ser enviado al back
+    //esta función completa el objeto del request para ser enviado al back
     function handleSeleccion(e) {
         setEleccion(e.target.value);
 
     }
 
     //Esta función me lleva a la pagina de food
-    function goToFood(){
+    function goToFood() {
         history.push({
             pathname: '/food',
-            response:response,
+            response: response,
         })
     }
 
     const consultaAPI = async () => {
         try {
-        const consulta = await axios({ method: 'POST', url: 'http://localhost:8080/usuario/login', data: request });
-      
-        setResponse(consulta);
-        goToFood();
+            const consulta = await axios(
+                { method: 'POST', url: 'http://localhost:8080/usuario/login', data: request }
+            );
 
-        
-       } catch (error) {
-           console.log(error)
-           setMensaje("Ingreso un usuario y/o clave invalido")
-       }
-        
-        
+            setResponse(consulta);
+            goToFood();
+
+
+        } catch (error) {
+            console.log(error)
+            setMensaje("Ingreso un usuario y/o clave invalido")
+        }
     }
 
 
     function _login(e) {
         e.preventDefault();
         if (eleccion === "paciente") {
-                consultaAPI();
+            consultaAPI();
         }
-        if(eleccion===""){
+        if (eleccion === "") {
             setMensaje("Debe seleccionar una opcion")
         }
-
-
     }
 
+    //captura el evento de lo que se escribe en los input y crea el request
     function _onChange(e) {
-        if(!mensaje==""){
+        if (!mensaje == "") {
             setMensaje("")
         }
-        
-
         const { id, value } = e.target;
         setRequest({ ...request, [id]: value })
-
-
     }
 
     return (
         <div>
-            <h1 className="text-center bg-primary text-white">Wellcome to login saludable-app</h1>
+            <h1 className="text-center bg-primary text-white">Welcome to login saludable-app</h1>
             <div className="container">
                 <div class="form-check">
                     <h4 className="">Seleccione una opcion</h4>
                     <input class="form-check-input" type="radio"
                         name="exampleRadios" id="exampleRadios1" value="personal"
-                        onClick={handleSeleccion}  />
+                        onClick={handleSeleccion} />
                     <label class="form-check-label" for="exampleRadios1">
                         personal de salud
                     </label>
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio"
-                        name="exampleRadios" id="exampleRadios2" value="paciente" 
-                        onClick={handleSeleccion}  />
+                        name="exampleRadios" id="exampleRadios2" value="paciente"
+                        onClick={handleSeleccion} />
                     <label class="form-check-label" for="exampleRadios2">
                         paciente
                  </label>
@@ -96,16 +91,8 @@ function Login() {
                     <button className=" btn btn-primary"
                         onClick={_login}>Ingresar</button>
                 </div>
-
             </div>
-
-
-
-
         </div>
-
-
-
     )
 
 }
